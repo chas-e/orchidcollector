0d6669ddff3ecfc7f9d5d820b0b26988788ff370
chas-e <sigmapi76388@gmail.com>
2020-09-08 21:27:03 -0400

    added detail functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('orchids/', views.orchids_index, name='index'),
+    path('orchids/<int:orchid_id>/', views.orchids_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Orchid
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -8,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def orchids_index(request):
+    orchids = Orchid.objects.all()
     return render(request, 'orchids/index.html', { 'orchids': orchids })
+
+def orchids_detail(request, orchid_id):
+    orchid = Orchid.objects.get(id=orchid_id)
+    return render(request, 'orchids/detail.html', { 'orchid': orchid })
