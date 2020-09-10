48a7bc05ec850d830724ae61c8085aecbabb9374
chas-e <sigmapi76388@gmail.com>
2020-09-10 15:02:56 -0400

    adds supply model and starts templates

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,18 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Orchid(models.Model):
+class Supply(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 250)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("supplies_detail", kwargs={ "supply_id": self.id })
+    
+    
+    class Orchid(models.Model):
     name = models.CharField(max_length = 100)
     genus = models.CharField(max_length = 100)
     description = models.TextField(max_length = 250)
@@ -13,5 +24,5 @@ class Orchid(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'orchid_id': self.id })
+        return reverse('detail', kwargs={ 'orchid_id': self.id })
     
